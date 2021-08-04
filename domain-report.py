9932864831fabe97627b17e41621e917d94e4375
abhilash <abhilashkb9020@gmail.com>
2021-08-04 12:41:57 +0530

    completed

--- domain-report.py
+++ domain-report.py
@@ -1,3 +1,4 @@
+import requests
 import openpyxl
 from openpyxl import cell
 import whois
@@ -17,28 +18,42 @@ for i in range(2, domains.max_row + 1):
         print(ipval.to_text())
         record=record+str(ipval)+"\n"
         
-    domains.cell(i,2).value=record
+    domains.cell(i,5).value=record
     MXrecord = resolver.resolve(domain,'mx')
     record=""
     for ipval in MXrecord:
         print(ipval.to_text())
         record=record+str(ipval)+"\n"
-    domains.cell(i,3).value=record
+    domains.cell(i,6).value=record
     
     TXTrecord = resolver.resolve(domain,'txt')
     record=""
     for ipval in TXTrecord:
         print(ipval.to_text())
         record=record+str(ipval)+"\n"
-    domains.cell(i,6).value=record
+    domains.cell(i,7).value=record
     
     dwhois=whois.whois(domain)
     #print(dwhois)
     #domains.cell(i,4).value=
-    print(dwhois['expiration_date'][0].strftime('%m/%d/%Y'))
-    domains.cell(i,5).value=str(dwhois['registrar'])
+   # print(dwhois['expiration_date'][1])
+    #print(dwhois['expiration_date'][1].strftime('%m/%d/%Y'))
+    domains.cell(i,3).value=str(dwhois['registrar'])
+    try:
+        expdate=str(dwhois['expiration_date'][1])
+        domains.cell(i,4).value=expdate
+    except:
+        print("no")
+    
+    rcode=str(requests.head('http://'+domain+'', headers={'User-Agent': 'Foo bar'},allow_redirects=True).status_code)
+    domains.cell(i,2).value=rcode
+    
+    try:
+        domains.cell(i,8).value = str(resolver.resolve("default._domainkey."+domain,'txt'))
+    except:
+        print("no")
+    
     
 
-
-
+    
 domainfile.save("solution.xlsx")    
--- import whois.py
+++
@@ -1,5 +0,0 @@
-import whois
-import datetime
-domain="google.com"
-dwhois=whois.whois(domain)
-print((dwhois['expiration_date'][1].strftime('%m/%d/%Y')))
